f071d29869dea986d1b1e09d7e4c53463ae7ee8c
Ka Po <kapo.chow@gmail.com>
2025-04-02 18:06:55 +0100

    input point data update

--- tools/data/map.js
+++ tools/data/map.js
@@ -1,66 +1,115 @@
 const mapData = [
   {
-    "id": "RF",
-    "name": "R/F",
-    "map": require('./map_images/RF.png'),
+    "id": "1F",
+    "name": "1/F",
+    "map": require('./map_images/1F.png'),
     "map_width": 1200,
-    "map_height": 499
-  },
-  {
-    "id": "URF",
-    "name": "UR/F",
-    "map": require('./map_images/URF.png'),
-    "map_width": 1200,
-    "map_height": 1597
-  },
-  {
-    "id": "GF",
-    "name": "G/F",
-    "map": require('./map_images/GF.png'),
-    "map_width": 1200,
-    "map_height": 558
-  },
-  {
-    "id": "B1",
-    "name": "B1/F",
-    "map": require('./map_images/B1.png'),
-    "map_width": 1200,
-    "map_height": 471
-  },
-  {
-    "id": "6-26F",
-    "name": "6F-26F",
-    "map": require('./map_images/6-26F.png'),
-    "map_width": 1200,
-    "map_height": 496
-  },
-  {
-    "id": "5F",
-    "name": "5/F",
-    "map": require('./map_images/5F.png'),
-    "map_width": 1200,
-    "map_height": 452
-  },
-  {
-    "id": "3F",
-    "name": "3/F",
-    "map": require('./map_images/3F.png'),
-    "map_width": 1200,
-    "map_height": 533
+    "map_height": 472
   },
   {
     "id": "2F",
     "name": "2/F",
     "map": require('./map_images/2F.png'),
     "map_width": 1200,
-    "map_height": 533
+    "map_height": 472
   },
   {
-    "id": "1F",
-    "name": "1/F",
-    "map": require('./map_images/1F.png'),
+    "id": "3F",
+    "name": "3/F",
+    "map": require('./map_images/3F.png'),
     "map_width": 1200,
-    "map_height": 529
+    "map_height": 472
+  },
+  {
+    "id": "11F",
+    "name": "11/F",
+    "map": require('./map_images/6-26.png'),
+    "map_width": 1200,
+    "map_height": 485
+  },
+  {
+    "id": "10F",
+    "name": "10/F",
+    "map": require('./map_images/6-26.png'),
+    "map_width": 1200,
+    "map_height": 485
+  },
+  {
+    "id": "15F",
+    "name": "15/F",
+    "map": require('./map_images/6-26.png'),
+    "map_width": 1200,
+    "map_height": 485
+  },
+  {
+    "id": "BF",
+    "name": "B1/F",
+    "map": require('./map_images/BF.png'),
+    "map_width": 1200,
+    "map_height": 512
+  },
+  {
+    "id": "BFA",
+    "name": "B1/F 食水水泵房（平台）",
+    "map": require('./map_images/BF.png'),
+    "map_width": 1200,
+    "map_height": 512
+  },
+  {
+    "id": "BFB",
+    "name": "B1/F 食水及咸水水缸水泵房（大樓）",
+    "map": require('./map_images/BF.png'),
+    "map_width": 1200,
+    "map_height": 512
+  },
+  {
+    "id": "BFC",
+    "name": "B1/F 消防及灑水泵房",
+    "map": require('./map_images/BF.png'),
+    "map_width": 1200,
+    "map_height": 512
+  },
+  {
+    "id": "GF",
+    "name": "G/F",
+    "map": require('./map_images/GF.png'),
+    "map_width": 1200,
+    "map_height": 544
+  },
+  {
+    "id": "RF1",
+    "name": "天台咸水水泵房",
+    "map": require('./map_images/RF.png'),
+    "map_width": 1200,
+    "map_height": 485
+  },
+  {
+    "id": "RF",
+    "name": "R/F",
+    "map": require('./map_images/RF.png'),
+    "map_width": 1200,
+    "map_height": 485
+  },
+  {
+    "id": "RF2",
+    "name": "天台食水水泵房",
+    "map": require('./map_images/RF.png'),
+    "map_width": 1200,
+    "map_height": 485
+  },
+  {
+    "id": "URF1",
+    "name": "UR1/F",
+    "map": require('./map_images/URF1.png'),
+    "map_width": 1200,
+    "map_height": 1219
+  },
+  {
+    "id": "URF2",
+    "name": "上層天台發電機房",
+    "map": require('./map_images/URF1.png'),
+    "map_width": 1200,
+    "map_height": 1219
   }
 ];
 export default mapData;

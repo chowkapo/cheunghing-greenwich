8eeb35279c1d2cdf2bb8d776636a3aa9c1d3a371
Ka Po <kapo.chow@gmail.com>
2025-04-02 17:27:53 +0100

    initial check-in of input point data and scripts

---
+++ tools/data/run_data.sh
@@ -0,0 +1,2 @@
+node make_inputpoint_data.js InputPoint.txt > input-point.json
+cp input-point.json ../../mobile/GreenwichApp/src/resources/data/input-point.json
---
+++ tools/data/run_map.sh
@@ -0,0 +1,2 @@
+node make_map_data.js InputPoint-Images.txt > map.js
+cp map.js ../../mobile/GreenwichApp/src/resources/data/map.js
